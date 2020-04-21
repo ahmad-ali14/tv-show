4057031f7382a2bbb578b375b7de99c0efcfbe6e
Ahmad Ali <aallii300300@gmail.com>
2020-04-21 22:21:19 +0100

    level 300 - search box

--- script.js
+++ script.js
@@ -2,26 +2,51 @@
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
+  const searchBox = document.getElementById('searchBox');
+  searchBox.addEventListener('keyup', doSearch)
 }
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
- 
- let epi = '';
- for(let i=0; i<episodeList.length; i++){
-  epi = epi +  `
+
+  let epi = '';
+  for (let i = 0; i < episodeList.length; i++) {
+    epi = epi + `
   <div class="third">
-  <h4 class= "epi-title"> ${episodeList[i].name} - S${ episodeList[i].season < 9 ? '0'+  episodeList[i].season :  episodeList[i].season  }E${ episodeList[i].number < 9 ? '0'+  episodeList[i].number :  episodeList[i].number  } </h4>
-  <img src=" ${ episodeList[i].image.medium } " />
-  ${ episodeList[i].summary }
+  <h4 class= "epi-title"> ${episodeList[i].name} - S${episodeList[i].season < 9 ? '0' + episodeList[i].season : episodeList[i].season}E${episodeList[i].number < 9 ? '0' + episodeList[i].number : episodeList[i].number} </h4>
+  <img src=" ${ episodeList[i].image.medium} " />
+  ${ episodeList[i].summary}
   </div>
   `;
- } 
- 
+  }
+
+  if (episodeList.length === 0) {
+    epi = '<p> sorry. No episodes with this Name or discreption </p>';
+  }
+
+  document.getElementById('itemsNumber').innerHTML= `<p> Episode Found : ${ episodeList.length }</p>`
+  rootElem.innerHTML = epi;
+
+}
+
+function doSearch(e) {
+
+  let allEpi = document.querySelectorAll('.third');
+  const data = getAllEpisodes();
+  let term = e.target.value.trim();
+  let filteredEpi = data.filter((e) => {
+    return e.name.toLowerCase().includes(term.toLowerCase()) || e.summary.toLowerCase().includes(term.toLowerCase())
+  })
+
+  allEpi.forEach((e) => {
+    e.style.display = 'none';
+  })
 
 
- rootElem.innerHTML = epi;
-  
+  makePageForEpisodes(filteredEpi);
+
+
+
 }
 
 window.onload = setup;
