507fe6c54f43ea5ded9e92d88c6c754528f51fae
Ahmad Ali <aallii300300@gmail.com>
2020-04-21 21:06:24 +0100

    level100

--- script.js
+++ script.js
@@ -6,7 +6,22 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+ 
+ let epi = '';
+ for(let i=0; i<episodeList.length; i++){
+  epi = epi +  `
+  <div class="third">
+  <h4 class= "epi-title"> ${episodeList[i].name} - S${ episodeList[i].season < 9 ? '0'+  episodeList[i].season :  episodeList[i].season  }E${ episodeList[i].number < 9 ? '0'+  episodeList[i].number :  episodeList[i].number  } </h4>
+  <img src=" ${ episodeList[i].image.medium } " />
+  ${ episodeList[i].summary }
+  </div>
+  `;
+ } 
+ 
+
+
+ rootElem.innerHTML = epi;
+  
 }
 
 window.onload = setup;
