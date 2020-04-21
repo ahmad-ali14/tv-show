a06929e1a53910a141e4da6d9a59bc369d3a6ef9
Ahmad Ali <aallii300300@gmail.com>
2020-04-21 23:33:50 +0100

    level 300 selcting episodes

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
   const searchBox = document.getElementById('searchBox');
-  searchBox.addEventListener('keyup', doSearch)
+  searchBox.addEventListener('keyup', doSearch);
+  const selectBox = document.getElementById('selectBox');
+  fillUpselections(allEpisodes, selectBox);
+   
 }
 
 function makePageForEpisodes(episodeList) {
@@ -11,9 +14,11 @@ function makePageForEpisodes(episodeList) {
 
   let epi = '';
   for (let i = 0; i < episodeList.length; i++) {
+    let epiTitle = ` ${episodeList[i].name} - S${episodeList[i].season < 9 ? '0' + episodeList[i].season : episodeList[i].season}E${episodeList[i].number < 9 ? '0' + episodeList[i].number : episodeList[i].number}`;
+
     epi = epi + `
-  <div class="third">
-  <h4 class= "epi-title"> ${episodeList[i].name} - S${episodeList[i].season < 9 ? '0' + episodeList[i].season : episodeList[i].season}E${episodeList[i].number < 9 ? '0' + episodeList[i].number : episodeList[i].number} </h4>
+  <div class="third" >
+  <h4 class= "epi-title" id="${ epiTitle }"> ${ epiTitle } </h4>
   <img src=" ${ episodeList[i].image.medium} " />
   ${ episodeList[i].summary}
   </div>
@@ -24,13 +29,12 @@ function makePageForEpisodes(episodeList) {
     epi = '<p> sorry. No episodes with this Name or discreption </p>';
   }
 
-  document.getElementById('itemsNumber').innerHTML= `<p> Episode Found : ${ episodeList.length }</p>`
+  document.getElementById('itemsNumber').innerHTML = `<p> Episods Found : ${episodeList.length}</p>`
   rootElem.innerHTML = epi;
 
 }
 
 function doSearch(e) {
-
   let allEpi = document.querySelectorAll('.third');
   const data = getAllEpisodes();
   let term = e.target.value.trim();
@@ -42,11 +46,51 @@ function doSearch(e) {
     e.style.display = 'none';
   })
 
-
   makePageForEpisodes(filteredEpi);
 
+}
 
+
+function fillUpselections(episodeList, rootElem){
+  
+
+  let epi = `<select id="epiSelector" autofocus="-1"  onChange=" handleSelect(this.value)"> <option value="All Episodes" selected="selected">All Episodes</option>`;
+  for (let i = 0; i < episodeList.length; i++) {
+    let epiTitle = ` ${episodeList[i].name} - S${episodeList[i].season < 9 ? '0' + episodeList[i].season : episodeList[i].season}E${episodeList[i].number < 9 ? '0' + episodeList[i].number : episodeList[i].number}`;
+    epi = epi + `
+    <option value="${ epiTitle }"> ${ epiTitle } </option> 
+    `;
+  }
+  epi += '</select>'
+
+  rootElem.innerHTML = epi;
 
 }
+
+function handleSelect(elm){
+  if(elm === "All Episodes"){
+    return window.location.href=""
+  }
+  else {
+    
+    const data = getAllEpisodes();
+    let allEpi = document.querySelectorAll('.third');
+
+    let filteredEpi = data.filter((e) => {
+      return elm.toLowerCase().includes(e.name.toLowerCase()) 
+    })
+  
+    allEpi.forEach((e) => {
+      e.style.display = 'none';
+    })
+
+   
+  
+    makePageForEpisodes(filteredEpi);
+
+  }
+  // window.location.href=elm
+}
+
 
 window.onload = setup;
